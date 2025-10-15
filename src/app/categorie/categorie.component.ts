684a5be49baa951a27bb659ef15006157d0eb559
BuuurnZ <florian91800@gmail.com>
2025-10-15 11:25:12 +0200

    barre de recherche

--- src/app/categorie/categorie.component.ts
+++ src/app/categorie/categorie.component.ts
@@ -9,10 +9,24 @@ import { CategoriesService } from '../shared/categorie/categories.service';
 })
 export class CategorieComponent implements OnInit {
   categories: any[] = this.categoriesService.categories;
+  filteredCategories: any[] = [];
 
   constructor(private categoriesService: CategoriesService) {}
-  
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredCategories = this.categories;
+      return;
+    }
+
+    const query = text.toLowerCase();
+    this.filteredCategories = this.categories.filter(
+      category => category?.category?.toLowerCase().includes(query)
+    );
+  }
   ngOnInit(): void {
     this.categoriesService.getCategories();
+    this.filteredCategories = this.categories;
   }
+
 }
