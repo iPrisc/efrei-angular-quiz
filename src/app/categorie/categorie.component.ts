abdaa9fac38425911bc156d59a702b3f07b142f2
iPrisc <florent.lelion@gmail.com>
2025-10-15 10:44:32 +0200

    cat

--- src/app/categorie/categorie.component.ts
+++ src/app/categorie/categorie.component.ts
@@ -8,6 +8,8 @@ import { CategoriesService } from '../shared/categorie/categories.service';
   styleUrl: './categorie.component.scss'
 })
 export class CategorieComponent implements OnInit {
+  categories: any[] = this.categoriesService.categories;
+
   constructor(private categoriesService: CategoriesService) {}
   
   ngOnInit(): void {
